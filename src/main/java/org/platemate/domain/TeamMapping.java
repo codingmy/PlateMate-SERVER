d255e5d7555af2f72170a8e4aac66644a28a40b7
Choi Min Young <codingmy@naver.com>
2024-05-21 23:58:11 +0900

    [feat/#3] TeamMapping 구현

---
+++ src/main/java/org/platemate/domain/TeamMapping.java
@@ -0,0 +1,30 @@
+package org.platemate.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Table(name = "teamMapping")
+public class TeamMapping {
+    @Column(name = "user_id")
+    Long userId;
+    @Column(name = "nickname")
+    String nickname;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_auth_id")
+    Long teamAuthId;
+
+    @Column(name = "latitude")
+    Long latitude;
+    @Column(name = "longtitude")
+    Long longtitude;
+}
