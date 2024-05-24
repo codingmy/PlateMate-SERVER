539a0ea7e7c2d9743d20c14dd626c90fa0be674f
Choi Min Young <codingmy@naver.com>
2024-05-24 21:41:28 +0900

    [feat/#11] 인증번호로 팀매핑 진행 API service 구현

--- src/main/java/org/platemate/service/UserService.java
+++ src/main/java/org/platemate/service/UserService.java
@@ -1,8 +1,12 @@
 package org.platemate.service;
 
+import org.platemate.domain.TeamMapping;
+import org.platemate.dto.request.GetTeamMappingRequest;
 import org.platemate.dto.request.PostUserDataRequest;
 import org.platemate.dto.response.PostUserDataResponse;
+import org.platemate.exception.ErrorMessage;
 import org.platemate.repository.UserRepository;
+import org.webjars.NotFoundException;
 
 public class UserService {
     private UserRepository userRepository;
@@ -13,5 +17,21 @@ public class UserService {
         Long userId = userRepository.postUserData(request.nickname(), request.latitude(), request.longtitude());
         Long teamAuthCode = userRepository.creatTeamData(userId);
         return new PostUserDataResponse(teamAuthCode);
+    }
+
+    //인증번호로 팀매핑 진행
+    public void getTeamMapping(GetTeamMappingRequest request) {
+        //db에 유저 정보 업로드
+        Long userId = userRepository.postUserData(request.nickname(), request.latitude(), request.longtitude());
+        TeamMapping teamMappingData = userRepository.getTeamDataByTeamCode(request.authCode());
+
+        //인증코드가 유효할 때(조회 가능, 매핑전)
+        if (teamMappingData != null && teamMappingData.getIsMapped() == false)
+            //db에 유저 매핑 업데이트
+            userRepository.updateTeamMapping(userId);
+
+        else
+            //팀매핑 정보 없을 때(인증번호가 잘못된 경우)
+            throw new NotFoundException(ErrorMessage.CODE_NOT_FOUND_EXCEPTION.getMessage());
     }
 }
