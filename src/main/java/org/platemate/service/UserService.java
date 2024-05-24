438d841593a763af31ad6032f07e09937a39edf6
Choi Min Young <codingmy@naver.com>
2024-05-24 21:40:43 +0900

    [feat/#11] 인증번호 발급 API service 로직 수정

--- src/main/java/org/platemate/service/UserService.java
+++ src/main/java/org/platemate/service/UserService.java
@@ -7,7 +7,11 @@ import org.platemate.repository.UserRepository;
 public class UserService {
     private UserRepository userRepository;
 
+    //유저가 팀 인증번호 발급요청시 로직
+    //팀 인증번호 담은 response 리턴
     public PostUserDataResponse postUserData(PostUserDataRequest request) {
-        return new PostUserDataResponse(userRepository.postUserData(request.nickname(), request.latitude(), request.longtitude()));
+        Long userId = userRepository.postUserData(request.nickname(), request.latitude(), request.longtitude());
+        Long teamAuthCode = userRepository.creatTeamData(userId);
+        return new PostUserDataResponse(teamAuthCode);
     }
 }
