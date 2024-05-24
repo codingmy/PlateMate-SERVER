52918c8132c80fb372eb63a699cde214f653925c
Choi Min Young <codingmy@naver.com>
2024-05-25 03:44:47 +0900

    [refactor/#12] 2인버전 중간지점 맛집 조회api controller 구현

--- src/main/java/org/platemate/controller/RestaurantController.java
+++ src/main/java/org/platemate/controller/RestaurantController.java
@@ -4,8 +4,8 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.platemate.dto.request.GetRestaurantListRequestV1;
 import org.platemate.dto.request.GetRestaurantListRequestV2;
-import org.platemate.dto.response.GetRestListFromKakaoResponse;
-import org.platemate.dto.response.GetRestaurantListResponse;
+import org.platemate.dto.request.PostUserDataRequest;
+import org.platemate.dto.response.GetKakaoAPIResponse;
 import org.platemate.service.KakaoAPIService;
 import org.platemate.service.UserService;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +25,12 @@ public class RestaurantController {
         return this.getRestListFromKakaoAPI(request.longtitude(), request.latitude());
     }
 
+    @GetMapping("/V2")
+    public GetKakaoAPIResponse getRestaurantListV2(@RequestBody GetRestaurantListRequestV2 request) {
+        List<Float> locationData = userService.getTeamUserMidPlaceData(request.teamAuthCode());
+        //바로 카카오open api 사용해서 서버통신해서 정보 받아오기
+        return this.getRestListFromKakaoAPI(locationData.get(0), locationData.get(1));
+    }
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     @ResponseBody
